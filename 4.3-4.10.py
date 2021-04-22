ee6d7ea5ed79d898b2016566a642c731bc6c8fd7
xiaozhu-sorce <790362227@qq.com>
2021-04-23 00:04:16 +0800

    test

---
+++ 4.3-4.10.py
@@ -0,0 +1,22 @@
+for num in range(1,21):
+	print(num)
+
+nums = list(range(1,1000001))
+for num in nums:
+	print(num)
+
+print(min(nums))
+print(max(nums))
+print(sum(nums))
+
+jishu = list(range(1,21,2))
+for ji in jishu:
+	print(ji)
+	
+div = [va*3 for va in range(1,11)]
+print (div)
+
+thr = list(range(1,11))
+for th in thr:
+	print(th**3)
+
